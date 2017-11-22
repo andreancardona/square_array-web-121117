172a730bddf8bf8c7fcf7a7ef03d041a3dd95a94
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 01:23:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,13 @@
 def square_array(array)
-  numbers = [1,2,3]
-  numbers.each do |element|
-    numbers.collect {(|x| x) * (|x| x) }  
+  new_array = []
+  array.each do |i|
+    new_array << i**2
+  end
+  new_array
 end
 
 basket = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
- 
+
 # Step 1,2,3,4,5
 basket.each do |apple|
     puts "Taking out #{apple}"
